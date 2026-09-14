c82ddc503fe55e6d80465f29f719b0394f037a8d
mohammedtarek245 <mohammedtarek2910@gmail.com>
2026-09-14 15:41:53 +0300

    Add config check application

--- config-check/lib/secrets.ts
+++ config-check/lib/secrets.ts
@@ -1,12 +1,16 @@
-import sodium from "libsodium-wrappers";
+import sodium from 'libsodium-wrappers-sumo';
 
 export async function encryptSecretForRepo(
   publicKeyBase64: string,
   secretValue: string
 ): Promise<string> {
+  // Wait for the WebAssembly binary to load completely
   await sodium.ready;
+  
   const binKey = sodium.from_base64(publicKeyBase64, sodium.base64_variants.ORIGINAL);
   const binSecret = sodium.from_string(secretValue);
   const encryptedBytes = sodium.crypto_box_seal(binSecret, binKey);
+  
   return sodium.to_base64(encryptedBytes, sodium.base64_variants.ORIGINAL);
 }
+  
